c5810b1555bb04a4188ef63905828a5e7069dcd2
bommukarthikreddy <bommukarthik.reddy@terralogic.com>
2026-09-12 11:39:15 +0530

    set

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,12 @@
 import { Router } from "express";
 import userRoutes from "./userRoutes";
-<<<<<<< HEAD
-=======
+
 import productRouter from "./productRoutes";
 
->>>>>>> karthik
 
 const router = Router();
 
 router.use("/user", userRoutes);
-<<<<<<< HEAD
-=======
 router.use("/products", productRouter)
->>>>>>> karthik
 
 export default router;
